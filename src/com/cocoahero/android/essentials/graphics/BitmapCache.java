f9e69d4e01d88eeec2ce5125c6697583f06eff24
Jonathan Baker <jonathanmbaker@me.com>
2012-11-12 21:09:05 -0500

    Add factory for creating BitmapCache instances based on memory class

--- src/com/cocoahero/android/essentials/graphics/BitmapCache.java
+++ src/com/cocoahero/android/essentials/graphics/BitmapCache.java
@@ -1,5 +1,7 @@
 package com.cocoahero.android.essentials.graphics;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.support.v4.util.LruCache;
 
@@ -12,22 +14,60 @@ import android.support.v4.util.LruCache;
  */
 public class BitmapCache extends LruCache<String, Bitmap> {
 
-    /**
-     * Constructs a new {@link BitmapCache} allowing you to specify the maximum
-     * amount of memory used by the cache.
-     * 
-     * @param bytes The maximum amount of memory in bytes this cache should use.
-     */
-    public BitmapCache(int bytes) {
-        super(bytes);
-    }
+	/**
+	 * Constructs a new {@link BitmapCache} allowing you to specify the maximum
+	 * amount of memory used by the cache.
+	 * 
+	 * @param bytes The maximum amount of memory in bytes this cache should use.
+	 */
+	public BitmapCache(int bytes) {
+		super(bytes);
+	}
 
-    /**
-     * {@inheritDoc android.util.LruCache}
-     */
-    @Override
-    protected int sizeOf(String key, Bitmap bitmap) {
-        return (bitmap.getRowBytes() * bitmap.getHeight());
-    }
+	/**
+	 * {@inheritDoc android.util.LruCache}
+	 */
+	@Override
+	protected int sizeOf(String key, Bitmap bitmap) {
+		return (bitmap.getRowBytes() * bitmap.getHeight());
+	}
+
+	/**
+	 * A factory for creating a {@link BitmapCache} based on the application's
+	 * memory class.
+	 */
+	public static class BitmapCacheFactory {
+
+		/**
+		 * Returns a new {@link BitmapCache} instance configured to use 1/8th of
+		 * the application's memory class.
+		 * 
+		 * @param context
+		 * @return a new {@link BitmapCache} instance configured to use 1/8th of
+		 *         the application's memory class.
+		 */
+		public static BitmapCache getCache(Context context) {
+			return getCache(context, 0.125f);
+		}
+
+		/**
+		 * Returns a new {@link BitmapCache} instance configured to use the
+		 * specified percentage of the application's memory class.
+		 * 
+		 * @param context
+		 * @param percentage A floating value between 1.0f and 0.0f.
+		 * @return a new {@link BitmapCache} instance configured to use the
+		 *         given percentage of the application's memory class.
+		 */
+		public static BitmapCache getCache(Context context, float percentage) {
+			ActivityManager mgr = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+
+			final int memoryClass = mgr.getMemoryClass();
+			final int cacheSize = (int) (1048576 * memoryClass * percentage);
+
+			return new BitmapCache(cacheSize);
+		}
+
+	}
 
 }
